c14d54900170c104b00f6a9b12974d49fb90bb91
Vinicius Santos Silva de Lima <viniciussslima@A205-15L.imd.local>
2019-04-24 17:56:12 -0300

    Construtor e destruto da classe funcionario

---
+++ src/Funcionario.cpp
@@ -0,0 +1,21 @@
+#include "Funcionario.h"
+
+using namespace std;
+
+Funcionario::Funcionario(int new_id, string new_nome, string new_cpf,
+	short new_idade, short new_tipo_sanguineo, char new_fator_rh,
+	string new_especialidade)
+{
+	m_id = new_id;
+	m_nome = new_nome;
+	m_cpf = new_cpf;
+	m_idade = new_idade;
+	m_tipo_sanguineo = new_tipo_sanguineo;
+	m_fator_rh = new_fator_rh;
+	m_especialidade = new_especialidade;
+}
+
+Funcionario::~Funcionario()
+{
+
+}
