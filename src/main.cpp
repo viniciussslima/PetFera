9a1f6c476581b644d1dc0d535e00d57a337d0c16
Vinicius Santos Silva de Lima <viniciussslima@A205-15L.imd.local>
2019-04-24 17:57:20 -0300

    main teste

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 #include <string>
 #include <vector>
 
+#include "Funcionario.h"
+
+using namespace std;
+
 int main(void)
 {
-	
+	Funcionario funcionario(1, "Fulano", "000.000.000.00",
+	25, 0, '-',
+	"Mamiferos");
 }
