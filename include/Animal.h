81b9c0a8b2e67bd296e454866a35273753cb68a7
Vinicius Santos Silva de Lima <viniciussslima@A205-16L.imd.local>
2019-05-06 18:35:13 -0300

    classe Animal

---
+++ include/Animal.h
@@ -0,0 +1,29 @@
+#ifndef  _H_ANIMAL_H_
+#define _H_ANIMAL_H_
+
+#include "Veterinario.h"
+#include "Tratador.h"
+
+using namespace std;
+
+class Animal
+{
+	protected:
+		int m_id;
+		string m_classe;
+		string m_nome_cientifico;
+		char m_sexo;
+		double m_tamanho;
+		string m_dieta;
+		Veterinario m_veterinario;
+		Tratador m_tratador;
+		string m_nome_batismo;
+	public:
+		Animal();
+		Animal(int, string, string, char,
+			double, string, Veterinario,
+			Tratador, string);
+		~Animal();
+};
+
+#endif
