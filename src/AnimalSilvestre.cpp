42ad12bcdd1a8415b55ab201bbb2edb77f554eb3
Vinicius Santos Silva de Lima <viniciussslima@A205-16L.imd.local>
2019-05-06 18:37:56 -0300

    Construtor e destrutor da classe Animal Silvestre

---
+++ src/AnimalSilvestre.cpp
@@ -0,0 +1,13 @@
+#ifndef _H_ANIMAL_SILVESTRE_H_
+#define _H_ANIMAL_SILVESTRE_H_
+
+AnimalSilvestre::AnimalSilvestre(string new_autorizacao_ibama)
+{
+	m_autorizacao_ibama = new_autorizacao_ibama;
+}
+
+AnimalSilvestre::AnimalSilvestre(){}
+
+AnimalSilvestre::~AnimalSilvestre(){}
+
+#endif
