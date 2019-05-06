f2f8b577068090532b62a0e1a83dedf8e6ca9354
Vinicius Santos Silva de Lima <viniciussslima@A205-16L.imd.local>
2019-05-06 18:33:40 -0300

    classe Tratador

--- include/Tratador.h
+++ include/Tratador.h
@@ -8,6 +8,8 @@ class Tratador : public Funcionario
 	private:
 		int m_nivel_de_seguranca;
 	public:
+		Tratador(int, string, string,
+	short, short, char,	string, int);
 		Tratador();
 		~Tratador();
 	
