9b36c44d3dd8258a122d0287312f4dff7f8ef903
Vinicius Santos Silva de Lima <viniciussslima@A205-16L.imd.local>
2019-05-06 18:35:29 -0300

    classe Animal Silvestre

---
+++ include/AnimalSilvestre.h
@@ -0,0 +1,13 @@
+#ifndef _H_ANIMAL_SILVESTRE_H_
+#define _H_ANIMAL_SILVESTRE_H_
+
+class AnimalSilvestre
+{
+	protected:
+		string m_autorizacao_ibama;
+	public:
+		AnimalSilvestre();
+		~AnimalSilvestre();
+};
+
+#endif
