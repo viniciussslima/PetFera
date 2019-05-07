b889ea15d6e1d4c51dcb2dc1dcc4c1bc6f937647
viniciussslima <viniciussslima999@gmail.com>
2019-05-07 19:17:31 -0300

    Menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-#include "Funcionario.h"
 
 using namespace std;
 
 int main(void)
 {
-	Funcionario funcionario(1, "Fulano", "000.000.000.00",
-	25, 0, '-',
-	"Mamiferos");
+	char escolha;
+	bool loop = true;
+
+	cout << "Digite um numero comrespondente: " << endl  
+		<< "[1] Cadastro de um animal" << endl
+		<< "[2] Remoção de um animal" << endl
+		<< "[3] Cadastro de um funcionario" << endl
+		<< "[4] Remoção de um funcionario" << endl
+		<< "[5] Consulta de dados de um animal" << endl
+		<< "[6] Consulta de dados de funcionario"
+		<< "[7] Sair" << endl;
+	cin >> escolha;
+	
+	while(loop)
+	{
+		switch(escolha)
+		{
+			case '1':
+				Cadastro_animal();
+				break;
+			case '2':
+				Remocao_animal();
+				break;
+			case '3':
+				Cadastro_funcionario();
+				break;
+			case '4':
+				Remocao_funcionario();
+				break;
+			case '5':
+				Consultar_animal();
+				break;
+			case '6':
+				Consultar_funcionario();
+				break;
+			case '7':
+				loop = false;
+				break
+			default:
+				cout << "Opção invalida" << endl;
+				break;
+		}
+	}
 }
