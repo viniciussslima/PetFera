15d09bf179c267c0ce9e8964722ce17dc9ee3da8
Hudson Bruno Macedo Alves <kotzuo@A307-13L.imd.local>
2019-05-07 16:12:35 -0300

    Classe Anfibio Exotico

---
+++ include/AnfibioExotico.h
@@ -0,0 +1,13 @@
+#ifndef _H_ANFIBIOEXOTICO_H
+#define _H_ANFIBIOEXOTICO_H
+
+using namespace std;
+
+class AnfibioExotico : public Anfibio
+{
+	public:
+		AnfibioExotico();
+		~AnfibioExotico();
+};
+
+#endif
