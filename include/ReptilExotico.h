7de2aa9dbf459f970a52d06f62a35b2df005da21
Hudson Bruno Macedo Alves <kotzuo@A307-13L.imd.local>
2019-05-07 16:16:21 -0300

    Classe Reptil Exotico

---
+++ include/ReptilExotico.h
@@ -0,0 +1,13 @@
+#ifndef _H_REPTILEXOTICO_H
+#define _H_REPTILEXOTICO_H
+
+using namespace std;
+
+class ReptilExotico : public Reptil
+{
+	public:
+		ReptilExotico();
+		~ReptilExotico();
+};
+
+#endif
