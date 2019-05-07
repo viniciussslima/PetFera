6b3134cd63374aca8bcc39183824d2aa5d9d68f6
viniciussslima <viniciussslima999@gmail.com>
2019-05-07 18:53:08 -0300

    include anfibio.h

--- include/AnfibioExotico.h
+++ include/AnfibioExotico.h
@@ -1,6 +1,8 @@
 #ifndef _H_ANFIBIOEXOTICO_H_
 #define _H_ANFIBIOEXOTICO_H_
 
+#include "Anfibio.h"
+
 using namespace std;
 
 class AnfibioExotico : public Anfibio
--- include/AnfibioNativo.h
+++ include/AnfibioNativo.h
@@ -1,6 +1,8 @@
 #ifndef _H_ANFIBIONATIVO_H_
 #define _H_ANFIBIONATIVO_H_
 
+#include "Anfibio.h"
+
 using namespace std;
 
 class AnfibioNativo : public Anfibio
