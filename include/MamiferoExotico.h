d91705e132d4ef4cf8028bc754719f61029f6d36
viniciussslima <viniciussslima999@gmail.com>
2019-05-07 18:53:59 -0300

    include Mamifero.h

--- include/MamiferoExotico.h
+++ include/MamiferoExotico.h
@@ -1,6 +1,8 @@
 #ifndef _H_MAMIFEROEXOTICO_H_
 #define _H_MAMIFEROEXOTICO_H_
 
+#include "Mamifero.h"
+
 using namespace std;
 
 class MamiferoExotico : public Mamifero
--- include/MamiferoNativo.h
+++ include/MamiferoNativo.h
@@ -1,6 +1,8 @@
 #ifndef _H_MAMIFERONATIVO_H_
 #define _H_MAMIFERONATIVO_H_
 
+#include "Mamifero.h"
+
 using namespace std;
 
 class MamiferoNativo : public Mamifero
