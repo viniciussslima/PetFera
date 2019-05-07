95e5b287f45d0bfdc116a4dfe4b9ae719ee6df69
João Vitor Kobata <jvkobata@A307-12L.imd.local>
2019-05-07 16:27:07 -0300

    Classe Mamifero

---
+++ include/Mamifero.h
@@ -0,0 +1,22 @@
+#ifdef MAMIFERO_H
+#define MAMIFERO_H
+#include "Animal.h"
+#include <string>
+using namespace std;
+
+class Mamifero: public Animal
+{
+protected:
+	string m_cor_pelo;
+
+public:
+	Mamifero();
+	~Mamifero();
+
+
+
+
+
+
+}
+#ifdef
