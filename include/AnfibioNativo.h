9ddc2e38d6fecf06465dd6129ab0bbd8bea198dd
Hudson Bruno Macedo Alves <kotzuo@A307-13L.imd.local>
2019-05-07 16:13:05 -0300

    Classe Anfibio Nativo

---
+++ include/AnfibioNativo.h
@@ -0,0 +1,13 @@
+#ifndef _H_ANFIBIONATIVO_H
+#define _H_ANFIBIONATIVO_H
+
+using namespace std;
+
+class AnfibioNativo : public Anfibio
+{
+	public:
+		AnfibioNativo();
+		~AnfibioNativo();
+};
+
+#endif
