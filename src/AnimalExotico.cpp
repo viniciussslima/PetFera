d680ffca27e643f3ee85d9c6ed06b9ac923e6b2c
Vinicius Santos Silva de Lima <viniciussslima@A307-11L.imd.local>
2019-05-07 16:09:13 -0300

    Construtor e destrutor da classe Animal Exotico

---
+++ src/AnimalExotico.cpp
@@ -0,0 +1,13 @@
+#include "AnimalExotico.h"
+
+using namespace std;
+
+
+AnimalExotico::AnimalExotico(){}
+
+AnimalExotico::AnimalExotico(string new_pais_origem)
+{
+	m_pais_origem = new_pais_origem;
+}
+
+AnimalExotico::~AnimalExotico(){}
