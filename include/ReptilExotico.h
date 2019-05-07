2302e947352c4a24cea251df7b79b7387dce014d
viniciussslima <viniciussslima999@gmail.com>
2019-05-07 18:54:30 -0300

    include Reptil.h

--- include/ReptilExotico.h
+++ include/ReptilExotico.h
@@ -1,6 +1,8 @@
 #ifndef _H_REPTILEXOTICO_H_
 #define _H_REPTILEXOTICO_H_
 
+#include "Reptil.h"
+
 using namespace std;
 
 class ReptilExotico : public Reptil
--- include/ReptilNativo.h
+++ include/ReptilNativo.h
@@ -1,6 +1,8 @@
 #ifndef _H_REPTILNATIVO_H_
 #define _H_REPTILNATIVO_H_
 
+#include "Reptil.h"
+
 using namespace std;
 
 class ReptilNativo : public Reptil
