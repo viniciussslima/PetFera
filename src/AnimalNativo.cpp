f37af2cc00d651279ab829882fe8a63fd7670128
Vinicius Santos Silva de Lima <viniciussslima@A307-11L.imd.local>
2019-05-07 16:09:35 -0300

    Construtor e destrutor da classe Animal Nativo

---
+++ src/AnimalNativo.cpp
@@ -0,0 +1,10 @@
+#include "AnimalNativo.h"
+
+AnimalNativo::AnimalNativo(){}
+
+AnimalNativo::AnimalNativo(string new_uf_origem)
+{
+	m_uf_origem = new_uf_origem;
+}
+
+AnimalNativo::~AnimalNativo(){}
