1bb5b34313d2bf24ef080b5f69430c0252d35454
Vinicius Santos Silva de Lima <viniciussslima@fc15b4d97188L.imd.local>
2019-05-08 17:52:27 -0300

    Adicionado uma quebra de linha

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,18 @@ int main(void)
 	char escolha;
 	bool loop = true;
 
-	cout << "Digite um numero comrespondente: " << endl  
-		<< "[1] Cadastro de um animal" << endl
-		<< "[2] Remoção de um animal" << endl
-		<< "[3] Cadastro de um funcionario" << endl
-		<< "[4] Remoção de um funcionario" << endl
-		<< "[5] Consulta de dados de um animal" << endl
-		<< "[6] Consulta de dados de funcionario"
-		<< "[7] Sair" << endl;
-	cin >> escolha;
-	
 	while(loop)
 	{
+		cout << "Digite um numero comrespondente: " << endl  
+			<< "[1] Cadastro de um animal" << endl
+			<< "[2] Remoção de um animal" << endl
+			<< "[3] Cadastro de um funcionario" << endl
+			<< "[4] Remoção de um funcionario" << endl
+			<< "[5] Consulta de dados de um animal" << endl
+			<< "[6] Consulta de dados de funcionario" << endl
+			<< "[7] Sair" << endl;
+		cin >> escolha;
+	
 		switch(escolha)
 		{
 			case '1':
