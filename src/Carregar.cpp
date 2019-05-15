7fb5b0d38ef79c74c89f5af21e3f60fcec151e9b
viniciussslima <viniciussslima999@gmail.com>
2019-05-15 12:44:29 -0300

    Função carregar

--- src/Carregar.cpp
+++ src/Carregar.cpp
@@ -3,47 +3,76 @@
 #include <vector>
 #include <fstream>
 
-#include "AnfibioNativo.h" 
-#include "AnfibioExotico.h" 
-#include "MamiferoNativo.h" 
-#include "MamiferoExotico.h" 
-#include "ReptilNativo.h" 
-#include "ReptilExotico.h" 
-#include "AveNativo.h" 
-#include "AveExotico.h" 
+#include "Animal.h"
 #include "Veterinario.h"
 #include "Tratador.h"
 #include "separador.h"
 
 using namespace std;
 
-void carregar (vector<AnfibioExotico> &AnfibiosExoticos&,
-	vector<AnfibioNativo> &AnfibiosNativos, 
-	vector<AveExotico> &AvesExoticos,
-	vector<AveNativo> &AvesNativos,
-	vector<MamiferoExotico> &MamiferosExoticos,
-	vector<MamiferoNativo> &MamiferosNativos,
-	vector<ReptilExotico> &RepteisExoticos,
-	vector<ReptilNativo> &RepteislNativos, 
-	vector<Tratador> &Tratadores,
-	vector<Veterinario> &Veterinarios)
+void carregar (vector<Animal> &Animais, 
+	vector<Funcionario> &Funcionarios)
 {
-	string files[] = {"animais", "funcionarios"}
+	int id;
+	string nome;
+	string cpf;
+	short idade;
+	string tipo_sanguineo;
+	char fator_rh;
+	string especialidade;
+	string classe;
+	string nome_cientifico;
+	char sexo;
+	float tamanho;
+	string dieta;
+	Veterinario veterinario;
+	Tratador tratador;
+	string nome_batismo;
+	string nascionalidade;
+	ifstream infile;
+	
+	string files[] = {"funcionarios", "animais"}
 	string data;
 	vector <string> dados;
 	for (int i = 0; i < 2; i++)
 	{
-		ifstream infile.open("../Dados/" + file[i] + ".csv")
+		infile.open("../Dados/" + file[i] + ".csv")
 		while(getline(infile, data))
 		{
 			separador(data, dados);
-			if (dados[1].compare("Amphibia"))
+
+			if (file[i].compare("funcionarios"))
 			{
-				if (dados[11].size() == 2)
-				{
-					AnfibioNativo anfibioNativo();
-					AnfibiosNativos.push_back(anfibioNativo);
-				}
+				id = stoi(dados[0]);
+				nome = dados[1];
+				cpf = dados[2];
+				idade = stoi(dados[3]);
+				tipo_sanguineo = dados[4];
+				fator_rh = dados[5];//
+				especialidade = dados[6];
+
+				Funcionario funcionario(id, nome, cpf,
+					idade, tipo_sanguineo, fator_rh
+					especialidade);
+				funcionarios.push_back(funcionario);
+			}
+			
+			else if (file[i].compare("animais"))
+			{
+				id = stoi(dados[0]);
+				classe = dados[1];
+				nome_cientifico = dados[2];
+				sexo = dados[3];//
+				tamanho = stod(dados[4]);
+				dieta = dados[5];
+				veterinario = dados[6];//
+				tratador = dados[7];//
+				nome_batismo = dados[8]
+
+				Animal animal(id, classe, nome_cientifico,
+					sexo, tamanho, dieta, veterinario,
+					tratador, nome_batismo);
+				Animais.push_back(animal);
 			}
 			dados.clear();
 		}
