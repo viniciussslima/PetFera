be5580acab2a5e2d8a2591adcb3e4d6ab50a5250
Kotzuo <hb.natal@gmail.com>
2019-05-18 16:09:28 -0300

    teste

---
+++ gtk/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#include <gtkmm.h>
+using namespace Gtk;
+
+#include "JanelaPrincipal.h"
+
+int main(int argc, char* argv[])
+{
+	Main kit(argc, argv);
+
+	JanelaPrincipal janelaPrincipal;
+	janelaPrincipal.run();
+}
