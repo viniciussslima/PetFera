82dd124cc154c98213526706f945dda623e1c7d6
Hudson Bruno Macedo Alves <hb.natal@gmail.com>
2019-05-21 17:10:16 -0300

    Implementada a função Cadastrar Funcionario

--- src/Programa.cpp
+++ src/Programa.cpp
@@ -68,7 +68,64 @@ void Programa::Remocao_animal()
 
 void Programa::Cadastro_funcionario()
 {
-	
+	int id;
+	string funcao;
+	string nome_do_funcionario;
+	string cpf;
+	short idade;
+	string tipo_sanguineo;
+	char rh;
+	string especialidade;
+	string crmv;
+	int nivel_de_seguranca;
+	ofstream outfile;
+
+	cout << "Id: ";
+	cin >> id;
+
+	cout << "Função: ";
+	cin >> funcao;
+	for (unsigned int i = 0; i < funcao.length(); i++) 
+		funcao[i] = toupper(funcao[i]);
+
+	cout << "Nome: ";
+	cin.ignore();
+	getline(cin, nome_do_funcionario);
+
+	cout << "cpf: ";
+	cin >> cpf;
+
+	cout << "Idade: ";
+	cin >> idade;
+
+	cout << "Tipo Sanguineo: ";
+	cin >> tipo_sanguineo;
+
+	cout << "rh: ";
+	cin >> rh;
+
+	cout << "Especialidade: ";
+	cin.ignore();
+	getline(cin, especialidade);
+
+	if(funcao.compare("TRATADOR") == 0)
+	{
+		cout << "Nivel de Segurança: ";
+		cin >> nivel_de_seguranca;
+
+		Tratador tratador(id, nome_do_funcionario, cpf, idade, tipo_sanguineo, rh, especialidade, nivel_de_seguranca);
+		outfile.open("../Dados/funcionarios.csv", ios::app);
+		outfile << tratador << endl;
+	}
+	if(funcao.compare("VETERINARIO") == 0)
+	{
+		cout << "crmv: ";
+		cin >> crmv;
+
+		Veterinario veterinario(id, nome_do_funcionario, cpf, idade, tipo_sanguineo, rh, especialidade, crmv);
+		outfile.open("../Dados/funcionarios.csv", ios::app);
+		outfile << veterinario << endl;
+	}
 }
 
 void Programa::Remocao_funcionario()
