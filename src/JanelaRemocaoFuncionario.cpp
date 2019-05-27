dcc42d452974860e78e90a4c6f4df3f698569c8c
Hudson Bruno Macedo Alves <hb.natal@gmail.com>
2019-05-27 16:16:51 -0300

    Agora somente funcionários cadastrados podem ser apagados, caso o id apresentado não for de um funcionário cadastrado irá apresentar uma janela informando isso, também foi adicionado um pequeno ícone informando se o id é válido ou não

--- src/JanelaRemocaoFuncionario.cpp
+++ src/JanelaRemocaoFuncionario.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 JanelaRemocaoFuncionario::JanelaRemocaoFuncionario(JanelaPrincipal &jptemp, map<int, Veterinario> &vtemp, map<int, Tratador> &ttemp)
 {
+	valid_id = false;
+
 	janela_principal = &jptemp;
 
 	veterinarios = &vtemp;
@@ -22,6 +24,11 @@ JanelaRemocaoFuncionario::JanelaRemocaoFuncionario(JanelaPrincipal &jptemp, map<
 	window->set_title("Remover Funcionário");
 	window->add(*box_principal);
 
+	pixbuf_check = Gdk::Pixbuf::create_from_file("icons/check.ico");
+	pixbuf_uncheck = Gdk::Pixbuf::create_from_file("icons/uncheck.ico");
+
+	entry_id->set_icon_from_pixbuf(pixbuf_uncheck);
+
 	box_principal->add(*box_dados);
 	box_principal->pack_start(*button_confirmar, PACK_SHRINK);
 
@@ -30,6 +37,7 @@ JanelaRemocaoFuncionario::JanelaRemocaoFuncionario(JanelaPrincipal &jptemp, map<
 
 	//Conexão
 	button_confirmar->signal_clicked().connect(sigc::mem_fun(*this, &JanelaRemocaoFuncionario::Remover));
+	entry_id->signal_changed().connect(sigc::mem_fun(*this, &JanelaRemocaoFuncionario::AtualizarIconeId));
 }
 
 
@@ -63,43 +71,89 @@ void JanelaRemocaoFuncionario::Run()
 
 void JanelaRemocaoFuncionario::Remover()
 {
-	string linha;
-	int id = stoi(entry_id->get_text());
-	vector<string> palavras;
-	ifstream funcionarios_csv("Dados/funcionarios.csv");
-
-	if(funcionarios_csv.is_open())
+	if(valid_id)
 	{
-		ofstream funcionarios_temp("Dados/TempFuncionario.csv");
-		while(getline(funcionarios_csv, linha))
+		string linha;
+		int id = stoi(entry_id->get_text());
+		vector<string> palavras;
+		ifstream funcionarios_csv("Dados/funcionarios.csv");
+
+		if(funcionarios_csv.is_open())
 		{
-			palavras = Separador_csv(linha);
-			if(stoi(palavras[0]) != id)
-				funcionarios_temp << linha << endl;
-			else
+			ofstream funcionarios_temp("Dados/TempFuncionario.csv");
+			while(getline(funcionarios_csv, linha))
 			{
-				if(palavras[1].compare("TRATADOR") == 0)
-				{
-					//janela_principal->AtualizarLista(0);
-					tratadores->erase(id);
-				}
+				palavras = Separador_csv(linha);
+				if(stoi(palavras[0]) != id)
+					funcionarios_temp << linha << endl;
 				else
 				{
-					//janela_principal->AtualizarLista(1);
-					veterinarios->erase(id);
+					if(palavras[1].compare("TRATADOR") == 0)
+					{
+						//janela_principal->AtualizarLista(0);
+						tratadores->erase(id);
+					}
+					else
+					{
+						//janela_principal->AtualizarLista(1);
+						veterinarios->erase(id);
+					}
 				}
 			}
+			remove("Dados/funcionarios.csv");
+			rename("Dados/TempFuncionario.csv", "Dados/funcionarios.csv");
+			funcionarios_temp.close();
 		}
-		remove("Dados/funcionarios.csv");
-		rename("Dados/TempFuncionario.csv", "Dados/funcionarios.csv");
-		funcionarios_temp.close();
+		else
+		{
+			MessageDialog dialog(*window, "Arquivo funcionarios.csv não encontrado.");
+			dialog.set_secondary_text("Provavelmente nenhum funcionário foi cadastrado, certifique-se que existe algum funcionário antes de remover.");
+	  		dialog.run();
+		}
 	}
 	else
 	{
-		MessageDialog dialog(*window, "Arquivo funcionarios.csv não encontrado.");
-		dialog.set_secondary_text("Provavelmente nenhum funcionário foi cadastrado, certifique-se que existe algum funcionário antes de remover.");
+		MessageDialog dialog(*window, "Funcionário com ID inexistente.");
+		dialog.set_secondary_text("Não foi encontrado nenhum funcionário com o id apresentado.");
   		dialog.run();
 	}
 
 	window->close();
+}
+
+void JanelaRemocaoFuncionario::AtualizarIconeId()
+{
+	string temp = entry_id->get_text();
+	bool is_numeric = true;
+	for(unsigned int i = 0; i < temp.size(); i++)
+	{
+		if(!isdigit(temp[i]))
+		{
+			is_numeric = false;
+			break;
+		}
+	}
+	if(is_numeric && !temp.empty())
+	{
+		int id = stoi(temp);
+		map<int, Veterinario>::iterator it_v = veterinarios->find(id);
+		map<int, Tratador>::iterator it_t = tratadores->find(id);
+
+		if(it_v != veterinarios->end() || it_t != tratadores->end())
+		{
+			valid_id = true;
+			entry_id->set_icon_from_pixbuf(pixbuf_check);
+		}
+		else
+		{
+			valid_id = false;
+			entry_id->set_icon_from_pixbuf(pixbuf_uncheck);
+		}
+	}
+	else
+	{
+		valid_id = false;
+		entry_id->set_icon_from_pixbuf(pixbuf_uncheck);
+	}
+	
 }
