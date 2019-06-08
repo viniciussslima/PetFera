b09686b063a84a73ec491026c2220e224f0acddb
viniciussslima <viniciussslima999@gmail.com>
2019-06-08 16:19:27 -0300

    Correção de erros de sintaxe

--- src/JanelaEditarFuncionario.cpp
+++ src/JanelaEditarFuncionario.cpp
@@ -69,7 +69,7 @@ JanelaEditarFuncionario::JanelaEditarFuncionario(JanelaPrincipal &jptemp, map<in
 	label_nivel_de_seguranca = new Label("Nivel de segurança: ");
 
 	//Configuração
-	window->set_title("Cadastrar Funcionário");
+	window->set_title("Editar Funcionário");
 	window->set_resizable(false);
 	window->add(*box_principal);
 
@@ -392,32 +392,29 @@ void JanelaEditarFuncionario::Remover()
 	vector<string> palavras;
 	ifstream funcionarios_csv("Dados/funcionarios.csv");
 
-	if(funcionarios_csv.is_open())
+	ofstream funcionarios_temp("Dados/TempFuncionario.csv");
+	while(getline(funcionarios_csv, linha))
 	{
-		ofstream funcionarios_temp("Dados/TempFuncionario.csv");
-		while(getline(funcionarios_csv, linha))
+		palavras = Separador_csv(linha);
+		if(stoi(palavras[0]) != id)
+			funcionarios_temp << linha << endl;
+		else
 		{
-			palavras = Separador_csv(linha);
-			if(stoi(palavras[0]) != id)
-				funcionarios_temp << linha << endl;
+			if(palavras[1].compare("TRATADOR") == 0)
+			{
+				tratadores->erase(id);
+				janela_principal->AtualizarLista(0);
+			}
 			else
 			{
-				if(palavras[1].compare("TRATADOR") == 0)
-				{
-					tratadores->erase(id);
-					janela_principal->AtualizarLista(0);
-				}
-				else
-				{
-					veterinarios->erase(id);
-					janela_principal->AtualizarLista(1);
-				}
+				veterinarios->erase(id);
+				janela_principal->AtualizarLista(1);
 			}
 		}
-		remove("Dados/funcionarios.csv");
-		rename("Dados/TempFuncionario.csv", "Dados/funcionarios.csv");
-		funcionarios_temp.close();
 	}
+	remove("Dados/funcionarios.csv");
+	rename("Dados/TempFuncionario.csv", "Dados/funcionarios.csv");
+	funcionarios_temp.close();
 }
 
 void JanelaEditarFuncionario::MudarFuncionario()
